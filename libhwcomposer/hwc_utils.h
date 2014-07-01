00bd15be8c5a31a6b3726a7219c887e4eb11c1e1
RonGokhale <cips173@gmail.com>
2014-07-01 19:44:18 +0530

    hwc: bring back fb support for primary for mdp303.
    Change-Id: I98cf0fdae6b9b1100c97ac319ab1c321f55fe507

--- libhwcomposer/hwc_utils.h
+++ libhwcomposer/hwc_utils.h
@@ -40,6 +40,9 @@
 
 //Fwrd decls
 struct hwc_context_t;
+#ifdef USE_MDP3
+struct framebuffer_device_t;
+#endif
 
 namespace ovutils = overlay::utils;
 
@@ -382,6 +385,9 @@ enum eAnimationState{
 struct hwc_context_t {
     hwc_composer_device_1_t device;
     const hwc_procs_t* proc;
+#ifdef USE_MDP3
+    framebuffer_device_t *mFbDev;
+#endif
 
     //CopyBit objects
     qhwc::CopyBit *mCopyBit[HWC_NUM_DISPLAY_TYPES];
